307dacb2de9dab6838b37fb94de9b0bf053947d9
Orane31 <orane.chaper@gmail.com>
2020-08-31 20:23:50 +0200

    db created

--- db/schema.rb
+++ db/schema.rb
@@ -57,7 +57,8 @@ ActiveRecord::Schema.define(version: 2020_08_31_132553) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_orders_on_user_id"
-    
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
